f278e07a5ffe515554948e516cf27a4bcde29e28
shivam0786tab <sachdeva.shivam1786@gmail.com>
2025-12-11 02:59:56 +0530

    Create deploy.groovy

---
+++ vars/deploy.groovy
@@ -0,0 +1,5 @@
+def call(String repoName,String imageTag){
+  withCredentials([usernamePassword(credentialsId: 'docker-hub-cred' , usernameVariable:'DOCKER_USER' , passwordVariable:'DOCKER_PASS')]){
+      sh "echo ${DOCKER_PASS} | docker login -u ${DOCKER_USER} --password-stdin"
+      sh "docker push ${env.DOCKER_USER}/${repoName}:${imageTag}"
+}
